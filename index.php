08bbb0e221c172c184abc625ddb0c32bd26d4d87
Sandrak0z <r1047763@student.thomasmore.be>
2025-10-22 18:06:12 +0200

    added: nav, categories, styling

--- index.php
+++ index.php
@@ -4,11 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VanHoecke</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
     
+  <div class="main">
+    <div class="navbar">
+      <div class="logo">🛒 LOGO</div>
+      <div class="center-nav">
+        <a href="#">Werken bij</a>
+        <a href="#">Services</a>
+        <a href="#">Producten</a>
+        <a href="#">Duurzaamheid</a>
+      </div>
+      <div class="right-nav">
+        <button>Aanmelden</button>
+        <button>🛍️</button>
+      </div>
+    </div>
+    
+  <div class="categorie">
+    <h3>Categorieën</h3>
+    <ul>
+      <li>Categorie 1</li>
+      <ul  class="subcategorie">
+        <li>Subcategorie</li>
+      </ul>
+      <li>Categorie 2</li>
+    </ul>
+  </div>
 
 
+    <div class="content">
+      <h1>Welkom bij onze webshop!</h1>
+      <p>Bekijk onze producten en ontdek onze duurzame services.</p>
+    </div>
+  </div>
 
 </body>
 </html>
+
