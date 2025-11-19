07f8aa06d7ee0a9cc14f6a1e89c90c4a5eb4c191
Sandrak0z <r1047763@student.thomasmore.be>
2025-11-19 20:39:58 +0100

    changed: logo + flex

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>VanHoecke</title>
-    <link rel="stylesheet" href="css/stylesheet.css">
-</head>
-<body>
-    
-  <div class="main">
-    <div class="navbar">
-      <div class="logo">🛒 LOGO</div>
+    <link rel="stylesheet" href="css/stylesheet.css" />
+  </head>
+  <body>
+    <nav class="navbar">
+      <div class="logo"><img src="img/logoVnhoecke.png" alt="logo" /></div>
       <div class="center-nav">
         <a href="#">Werken bij</a>
         <a href="#">Services</a>
@@ -21,26 +19,27 @@
         <button>Aanmelden</button>
         <button>🛍️</button>
       </div>
+    </nav>
+
+    <div class="main">
+      <div class="categorie">
+        <h3>Categorieën</h3>
+        <ul>
+          <li>
+            Categorie 1
+            <ul class="subcategorie">
+              <li>Subcategorie</li>
+            </ul>
+          </li>
+
+          <li>Categorie 2</li>
+        </ul>
+      </div>
+
+      <div class="content">
+        <h1>Welkom bij onze webshop!</h1>
+        <p>Bekijk onze producten en ontdek onze duurzame services.</p>
+      </div>
     </div>
-    
-  <div class="categorie">
-    <h3>Categorieën</h3>
-    <ul>
-      <li>Categorie 1</li>
-      <ul  class="subcategorie">
-        <li>Subcategorie</li>
-      </ul>
-      <li>Categorie 2</li>
-    </ul>
-  </div>
-
-
-    <div class="content">
-      <h1>Welkom bij onze webshop!</h1>
-      <p>Bekijk onze producten en ontdek onze duurzame services.</p>
-    </div>
-  </div>
-
-</body>
+  </body>
 </html>
-
